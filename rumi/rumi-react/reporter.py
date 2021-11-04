1efa1c463e25974f42cd130ee7297d64d458c840
tl6kk <litianshuli@hotmail.com>
2021-11-04 19:38:36 +0800

    translation monitoring for react app

--- rumi/rumi-react/reporter.py
+++ rumi/rumi-react/reporter.py
@@ -1,11 +1,11 @@
 # rumi.rumi-react.reporter
-# Git history parser for monitoring react app translation status with lingui.js
+# Reporter for monitoring react app translation status with lingui.js
 #
 # Author: Tianshu Li
 # Created: Nov. 3 2021
 
 """
-Git history parser for monitoring react app translation status with lingui.js
+Reporter for monitoring react app translation status with lingui.js
 """
 
 ##########################################################################
@@ -13,7 +13,10 @@ Git history parser for monitoring react app translation status with lingui.js
 ##########################################################################
 
 
+import os
+import git
 import glob
+import shutil
 import textwrap
 import argparse
 
@@ -26,14 +29,83 @@ from tabulate import tabulate
 
 
 class ReactReporter():
-    def __init__(self, repo_url="", content_path="locales/"):
+    """
+    ReactReporter gets the translation needs from the react app repository
+    set up with lingui.js, and report the needs in either stats mode, E.g.:
+        | Language   |   Total |   Missing |   Word Count |
+        |------------+---------+-----------+--------------|
+        | ja         |     175 |       174 |         1204 |
+        | zh         |     175 |         1 |           41 |
+        | de         |     175 |         1 |           41 |
+        | fr         |     175 |         1 |           41 |
+        | en         |     175 |         0 |            0 |
+    or detail mode, E.g.:
+        ----------------------------------------------------------------------
+        ja Missing: 2
+        msgid1
+        msgid2
+        ----------------------------------------------------------------------
+        zh Missing: 0
+        ----------------------------------------------------------------------
+        de Missing: 0
+        ----------------------------------------------------------------------
+        fr Missing: 1
+        msgid1
+        ----------------------------------------------------------------------
+        en Missing: 0
+    
+    It also contains download_needs function to download translation needs 
+    into a .txt file for each language; and a insert_done function that inserts
+    translated messages back into the po files. 
+
+    Parameters
+    ----------
+    repo_url: string, default: ""
+        Url for cloning the repository for translation monitoring.
+    repo_path: string, default: ""
+        Path to a local folder that contains the react app project.
+    content_path: string, default: "locales/"
+        Path from the root of the repository to the directory that contains
+        contents that require translation.
+    """
+    def __init__(self, repo_url="", repo_path="", content_path="locales/"):
+        message = "Please provide either a remote repository url or the path to a local repository"
+        assert not (repo_url!="" and repo_path!=""), message
+
         self.repo_url = repo_url
-        self.repo_name = "repo"
-        # self.repo_path = self.repo_name+"/"
-        self.repo_path = repo_url
+        if repo_url != "":
+            self.repo_name = "repo"
+            self.repo_path = self.repo_name+"/"
+            self.clone_repo()
+        else:
+            self.validate_repo(repo_path)
+        
         self.content_path = content_path
 
+    def validate_repo(self, repo_path):
+        """
+        Check if the user-provided repo_path is a valid directory, if not, check
+        if it is the folder name of the repository. Otherwise, message user to
+        provide a valid repo_path.
+        Parameters
+        ----------
+        repo_path: string, default: ""
+            Path to the repository to monitory the translation status. Default
+            uses the current path. 
+        """
+        if os.path.isdir(repo_path):
+            if repo_path[-1] != "/":
+                self.repo_path = repo_path+"/"
+            else:
+                self.repo_path = repo_path
+        else:
+            print("Please specify a valid repository path")
+            self.repo_path = None 
+
     def clone_repo(self):
+        """
+        Clone the repository from a user-provided url.
+        """
         if os.path.isdir(self.repo_path):
             shutil.rmtree(self.repo_path)
         
@@ -41,6 +113,21 @@ class ReactReporter():
         git.Repo.clone_from(self.repo_url, self.repo_name)
 
     def get_needs(self):
+        """
+        Parse the po file for each language and extract msgid that are missing
+        translations.
+
+        Returns
+        -------
+        needs: dictionary 
+            {
+                lang: [list of msgid that needs to be translated]
+            }
+        msg_cnt: dictionary
+            {
+                lang: total number of msgid (int)
+            }
+        """
         needs, msg_cnt = {}, {}
         for po_file in glob.glob(self.repo_path+self.content_path+"/*/*.po"):
             lang = po_file.split("/")[-2]
@@ -56,11 +143,76 @@ class ReactReporter():
                         needs[lang].append(msgid)
         return needs, msg_cnt
 
-    def download_needs(self):
+    def download_needs(self, path="./"):
+        """
+        Writes the msgid that needs to be translated into a .txt file for each
+        language.
 
-    def insert_done(self):
+        Parameters
+        ----------
+        path: string, default: "./"
+            Path to where the .txt file are wrote to. Default writing to the
+            current path.
+        """
+        needs, _ = self.get_needs()
+        for lang in needs:
+            cnt = sum([len(s.split(" ")) for s in needs[lang]])
+            filename = "{}_needing_translation.txt".format(lang)
+            print("Creating file {}{}".format(path, filename))
+            with open(filename, "w+") as f:
+                header = "Language: {}, {} words needing translation \n".format(lang, cnt)
+                f.write(header)
+                for msg in needs[lang]:
+                    f.write("msgid "+msg+"\n")
+                    f.write('msgstr ""\n')
+
+    def insert_done(self, file, lang):
+        """
+        Combine new translations together with other translations in the po file 
+        of a language and generate a new file locally.
+
+        Parameters
+        ----------
+        file: string
+            Path to the file that contains the new translations.
+        lang: string 
+            Language of the translations, e.g. de, fr.
+            This lang needs to be consistent with the lingui.js locale setup.
+        """
+        insert = {}
+        with open (file, "r+") as f_insert:
+            for line in f_insert.readlines():
+                line = line.strip()
+                if line.startswith("msgid "):
+                    msgid = line.replace("msgid ", "")
+                if line.startswith("msgstr "):
+                    msgstr = line.replace("msgstr ", "")
+                    insert[msgid] = msgstr
+        po_file = glob.glob("{}{}{}/*.po".format(
+            self.repo_path, self.content_path, lang
+        ))[0]
+        
+        new_file = os.path.dirname(file)+"inserted_"+os.path.basename(file)
+        f_new = open(new_file, "w+")
+        with open(po_file, "w+") as f_old:
+            for line in f_old.readlines():
+                line = line.strip()
+                if line.startswith("msgid "):
+                    msgid = line.replace("msgid ", "")
+                    f_new.write(line)
+                elif line.startswith("msgstr "):
+                    if msgid in insert:
+                        msgstr = insert[msgid]
+                    f_new.write("msgstr "+msgstr+"\n")
+                else:
+                    f_new.write(line)
+        f_new.close()
 
     def stats(self, needs, msg_cnt):
+        """
+        Print out a summary of the translation status including number of message
+        missing and word count.
+        """
         data = []
         for lang in needs:
 
@@ -75,7 +227,11 @@ class ReactReporter():
             tablefmt="orgtbl"
         ))
     
-    def detail(self, needs, msg_cnt):
+    def detail(self, needs):
+        """
+        Print out the details of messages needing translations for each language
+        and provide word count.
+        """
         for lang in needs:
             print("-"*70)
             print(lang, "Missing:", len(needs[lang]))
@@ -86,20 +242,26 @@ class ReactReporter():
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument(
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument(
         '--repo_url', type=str, default="", 
         help='Please specify the url of the repository for translation monitoring'
     )
+    group.add_argument(
+        '--repo_path', type=str, default="",
+        help='Please specify the path to the repository'
+    )
     parser.add_argument(
-        '--content_path', type=str, default='content/', 
+        '--content_path', type=str, default='locales/', 
         help='Please specify the path from the root of repository to the content to be translated'
     )
 
     config = parser.parse_args()
 
     reporter = ReactReporter(
-        repo_url="../directory/",
-        content_path="web/gds-ui/src/locales"
+        repo_url=config.repo_url,
+        repo_path=config.repo_path,
+        content_path=config.content_path
     )
 
     needs, msg_cnt = reporter.get_needs()
