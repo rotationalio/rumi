b85ae136f6a38b1c2645433a5e6eb7f1d4612ada
tl6kk <litianshuli@hotmail.com>
2021-11-11 19:00:07 +0800

    add docstrings for version

--- rumi/version.py
+++ rumi/version.py
@@ -1,9 +1,39 @@
+# rumi.version
+# Maintain caches for git history reader
+#
+# Author: Tianshu Li
+# Created: Nov.11 2021
+
+"""
+Maintain caches for git history reader
+"""
+
+##########################################################################
+# Imports
+##########################################################################
+
+
 import os
 import pickle
 
 from datetime import datetime as dt
 
+
+##########################################################################
+# Class Version
+##########################################################################
+
+
 class Version():
+    """
+    Maintain caches for git history reader to load latest cache and read 
+    git history since latest cache.
+
+    Parameters
+    ----------
+    repo_name: string
+        Name of the repository for translation monitoring.
+    """
     def __init__(self, repo_name) -> None:
         self.repo_name = repo_name
         self.date_format = '%Y-%m-%d %H:%M:%S'
@@ -13,6 +43,18 @@ class Version():
         self.latest_version, self.latest_date = self.get_latest()
 
     def get_latest(self):
+        """
+        Get the version and date of the latest cache from cache folder. If no
+        cache folder or cache file, return version 0 and timestamp "1900-1-1 00:00:00".
+
+        Returns
+        -------
+        version: int
+            Version of the cache commit history.
+        date: string
+            Timestamp of the cache commit history in the format of  
+            "yyyy-mm-dd HH:MM:SS"
+        """
         dir = os.path.join(self.cache_dir, self.repo_name)
         if not os.path.isdir(dir):
             os.mkdir(dir)
@@ -32,18 +74,60 @@ class Version():
         return latest_version, latest_date
 
     def parse_cache_name(self, name):
+        """
+        Utility function to parse the name of cache file for version and date.
+        Parameters
+        ----------
+        name: string
+            Name of the cache file.
+
+        Returns
+        -------
+        version: int
+            Version of the cache commit history.
+        date: string
+            Timestamp of the cache commit history in the format of  
+            "yyyy-mm-dd HH:MM:SS"
+        """
         splits = name.split(" ")
         version = splits[0][1:]
         date = dt.strptime(" ".join(splits[1:]), self.date_format)
         return version, date
     
     def name_cache(self, version, date):
+        """
+        Utility function to compose the name of cache file given version and 
+        date.
+
+        Parameters
+        ----------
+        version: int
+            Version of the cache commit history.
+        date: string
+            Timestamp of the cache commit history in the format of  
+            "yyyy-mm-dd HH:MM:SS"
+
+        Returns
+        -------
+        name: string
+            Name of the cache file.
+        """
         name = os.path.join(
             self.cache_dir, self.repo_name, "v"+str(version)+" "+date
         )
         return name
 
     def write_cache(self, commits):
+        """
+        Check if the current commit history is different from the latest cache. 
+        If so, write new cache version; Otherwise, update the filename of the 
+        latest cache to reflect new timestamp.
+
+        Parameters
+        ----------
+        commits: dictionary
+            Current commit history from git reader.
+        """
         date = dt.now().strftime(self.date_format)
 
         old_file = self.name_cache(self.latest_version, self.latest_date)
@@ -64,6 +148,27 @@ class Version():
                 pickle.dump(commits,f)
 
     def load_cache(self):
+        """
+        Load cached git history.
+
+        Returns
+        -------
+        commits: dictionary
+            Commit history of the repository organized by 
+            {
+                "basename": {
+                    "filename": {
+                        "lang": "language of this file",
+                        "ft": timestamp of the first commit (float),
+                        "lt": timestamp of the last commit (float),
+                        "history": {
+                            timestamp (float): [#additions, #deletions]
+                        }
+                    }
+                }
+            }
+            The basename is the name of the content that is common among languages.
+        """
         file = os.path.join(
             self.cache_dir, self.repo_name, 
             "v"+str(self.latest_version)+" "+self.latest_date
